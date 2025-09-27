c609b7ec64f527277fe83fa95276832442b71cd3
Saad Srabon <saadsrabon2@gmail.com>
2025-09-28 05:13:03 +0600

    files uploaded

--- ItechParkBackend/controllers/contactController.js
+++ ItechParkBackend/controllers/contactController.js
@@ -72,3 +72,4 @@ module.exports = { submitMessage, getAllMessages, getMessageById, updateStatus, 
 
 
 
+
--- ItechParkBackend/models/ContactMessage.js
+++ ItechParkBackend/models/ContactMessage.js
@@ -15,3 +15,4 @@ module.exports = mongoose.model('ContactMessage', ContactMessageSchema);
 
 
 
+
